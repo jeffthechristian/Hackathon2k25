76ff8e2883ab962b7447d4455cb317da6dec27c5
Markus Vilcāns <markusvilcans@gmail.com>
2025-06-14 08:56:37 +0300

    Simple ring logic WIP. Organized scene

---
+++ Assets/RingLogic.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RingLogic : MonoBehaviour
+{
+    [SerializeField] private float maxHealth = 500f;
+    private float currentHealth;
+
+    private void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void TakeDamage(float amount)
+    {
+        currentHealth -= amount;
+        Debug.Log($"Ring took {amount} damage. Remaining Health: {currentHealth}");
+
+        if (currentHealth <= 0f)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
