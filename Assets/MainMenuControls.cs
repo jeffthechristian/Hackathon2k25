57fa9d8a0d35983302a8ed0de377f989a727b230
Markus Vilcāns <markusvilcans@gmail.com>
2025-06-15 01:05:09 +0300

    Added main menu/restart

---
+++ Assets/MainMenuControls.cs
@@ -0,0 +1,39 @@
+using Oculus.Interaction.Samples;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+public class MainMenuControls : MonoBehaviour
+{
+    public List<AudioClip> helpTroll;
+    private AudioSource audioSource;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        if (audioSource == null)
+            audioSource = gameObject.AddComponent<AudioSource>();
+    }
+    public void OnStart()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+    public void QuitApp()
+    {
+        Application.Quit();
+    }
+    public void PlaySound()
+    {
+        if (helpTroll == null || helpTroll.Count == 0)
+            return;
+
+        int index = Random.Range(0, helpTroll.Count);
+        AudioClip clip = helpTroll[index];
+
+        if (clip != null)
+        {
+            audioSource.PlayOneShot(clip);
+        }
+    }
+}
