dd22a51ba53f3d25a9deca7ce20abbc78e680275
Markus Vilcāns <markusvilcans@gmail.com>
2025-06-14 18:23:39 +0300

    stuff?

--- Assets/Scripts/TurretStuff/TurretBullet.cs
+++ Assets/Scripts/TurretStuff/TurretBullet.cs
@@ -1,8 +1,20 @@
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class TurretBullet : MonoBehaviour
 {
     public float damage = 10f;
+    private AudioSource audioSource;
+    public AudioClip shootSFX;
+
+    public void Start()
+    {
+        if (audioSource != null && shootSFX != null)
+        {
+            audioSource.pitch = Random.Range(0.6f, 1.4f);
+            audioSource.PlayOneShot(shootSFX);
+        }
+    }
     void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Enemy"))
